7402f836d911bcc9057a9d87e59063bfb8ee53c2
nick22985 <nick22985@hotmail.com>
2022-01-04 16:22:28 +1000

    Correcting github page build and also tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ const colors = require("tailwindcss/colors")
 
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/**/*.html"],
-	purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/**/*.html"],
 	darkMode: "class",
 	theme: {},
 	plugins: [],
